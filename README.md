58f110370ee7f039ad92f6a9bb05e1b79b0037e5
Adam Ghadban <ghadbanadam@Adams-MacBook-Pro-3.local>
2024-12-12 11:52:09 +0200

    added part defined running and README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 048891_hw1
+
+cd src
+pip3 install matplotlib
+python3 src/main.py --part <the part u want to run>
+
+
--- src/main.py
+++ src/main.py
@@ -3,10 +3,24 @@ import matplotlib.pyplot as plt
 import math
 import time
 import random
+import argparse
+
 
 n = 20343797
 num_bits = 4096
 random.seed(42)
+
+parser = argparse.ArgumentParser(description="Process an integer part argument.")
+parser.add_argument(
+    "--part", 
+    "-p", 
+    type=int, 
+    help="An integer argument representing the part to run (required)"
+)
+args = parser.parse_args()
+
+print(f"Running part {args.part}")
+
 
 def basic_l2r_square_and_multiply(a, e, n, w_s = 0, w_m = 0):
     s = len(e) # num of bits
@@ -71,7 +85,7 @@ def generate_number_with_hamming_weight(bit_length, hamming_weight):
 
 def part1():
     powers_of_2 = [2**i for i in range(1, num_bits + 1)]
-    num_bits = [math.log2(power_of_2) for power_of_2 in powers_of_2]
+    num_bits_tmp = [math.log2(power_of_2) for power_of_2 in powers_of_2]
     times = []
     t = 0
     avg = 20
@@ -84,11 +98,11 @@ def part1():
             t = t + total_time
         t = t / avg
         times.append(t)
-    plt.plot(num_bits, times)
+    plt.plot(num_bits_tmp, times)
     plt.xlabel("Num Bits, S")
     plt.ylabel("Execution Time")
     plt.title("Execution time vs S")
-    plt.xlim(1, num_bits[-1])
+    plt.xlim(1, num_bits_tmp[-1])
     plt.ylim(0, max(times))
     plt.savefig("part1.png")
 
@@ -198,7 +212,7 @@ def part5(part):
     orig_value = inject_dummy_l2r_square_and_multiply(2, e, n)
     key_bits = []
     for i in range(0, num_bits + 1):
-        if part == 4:
+        if part == 5:
             observed_val = inject_dummy_l2r_square_and_multiply(2, e, n, inject_fault_at_i=i)
             if observed_val == orig_value:
                 key_bits.append(0)
@@ -223,9 +237,15 @@ def part6():
     part5(6)
 
 if __name__ == "__main__":
-    #part1()
-    #part2()
-    #part3()
-    #part4()
-    #part5()
-    part6()
+    if args.part == 1:
+        part1()
+    elif args.part == 2:
+        part2()
+    elif args.part == 3:
+        part3()
+    elif args.part == 4:
+        part4()
+    elif args.part == 5:
+        part5(5)
+    elif args.part == 6:
+        part6()
